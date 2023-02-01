1d1b5c01dfc672d3b2c3d612b958e5dfd989fbd7
artonn2k <arton.bilalli1@gmail.com>
2023-02-01 18:58:29 +0100

    updated review & payment form/component,created myOrders page&details

--- client/src/app/components/AppCheckbox.tsx
+++ client/src/app/components/AppCheckbox.tsx
@@ -3,6 +3,7 @@ import { useController, UseControllerProps } from "react-hook-form"
 
 interface Props extends UseControllerProps{
     label: string;
+    disabled: boolean;
 }
 
 function AppCheckbox(props: Props) {
@@ -15,6 +16,7 @@ function AppCheckbox(props: Props) {
                 {...field}
                 checked={field.value}
                 color='secondary'
+                disabled={props.disabled}
             />
         }
         label={props.label}
