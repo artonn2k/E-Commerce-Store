6d8f7574af690f4d63d850d70721948d2dee59e6
artonn2k <arton.bilalli1@gmail.com>
2023-04-06 23:14:40 +0200

    added docker to use postgreSQL as production db, added new migrations to remove the sqlite annotations and add Npgsql annotations

--- API/Program.cs
+++ API/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
 
 
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
 builder.Services.AddCors();
