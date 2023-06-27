6cc24329fc0f4d122c1f53e305f9afd12c13064e
artonn2k <arton.bilalli1@gmail.com>
2023-06-27 16:31:13 +0200

    added image service(cloudinary)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,21 +9,23 @@ namespace API.Controllers
     {
         private readonly StoreContext _context;
         private readonly IMapper _mapper;
+        private readonly ImageService _imageService;
 
-        public ProductsController(StoreContext context, IMapper mapper)
-        {   
+        public ProductsController(StoreContext context, IMapper mapper, ImageService imageService)
+        {
+            _imageService = imageService;
             _mapper = mapper;
             _context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
         {
             var query = _context.Products
                 .Sort(productParams.OrderBy)    // .Sort -> the methods that comes from ProductExtensions class
                 .Search(productParams.SearchTerm)  // .Search -> the methods that comes from ProductExtensions class
                 .Filter(productParams.Brands, productParams.Types)  // .Filter -> the methods that comes from ProductExtensions class
-                .AsQueryable(); 
+                .AsQueryable();
 
             var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
 
@@ -33,14 +35,14 @@ namespace API.Controllers
 
             //filter for ordering -> price low to high , high to low, alphabetical order, searching product
         }
-       
+
         //this one is going to get from the root ex. api/produktet/3 - 3shi means ID produktit
-        [HttpGet("{id}", Name = "GetProduct")]   
-        public  async Task<ActionResult<Product>> GetProduct(int id)
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
 
-            if(product == null) return NotFound();
+            if (product == null) return NotFound();
 
             return product;
         }
@@ -51,40 +53,66 @@ namespace API.Controllers
             var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
             var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
 
-            return Ok(new{brands, types});
+            return Ok(new { brands, types });
         }
 
         [Authorize(Roles = "Admin")]
         [HttpPost]
-        public async Task<ActionResult<Product>> CreateProduct(CreateProductDto productDto)
+        public async Task<ActionResult<Product>> CreateProduct([FromForm] CreateProductDto productDto)
         {
             var product = _mapper.Map<Product>(productDto);
+
+            if (productDto.File != null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if (imageResult.Error != null)
+                    return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.PublicId = imageResult.PublicId;
+            }
 
             _context.Products.Add(product);
 
 
             var result = await _context.SaveChangesAsync() > 0;
 
-            if(result) return CreatedAtRoute("GetProduct", new {Id = product.Id}, product);
+            if (result) return CreatedAtRoute("GetProduct", new { Id = product.Id }, product);
 
-            return BadRequest(new ProblemDetails{Title="Error while creating new product"});
+            return BadRequest(new ProblemDetails { Title = "Error while creating new product" });
         }
 
         [Authorize(Roles = "Admin")]
         [HttpPut]
-        public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+        public async Task<ActionResult<Product>> UpdateProduct([FromForm]UpdateProductDto productDto)
         {
             var product = await _context.Products.FindAsync(productDto.Id);
 
-            if(product == null) return NotFound();
+            if (product == null) return NotFound();
 
             _mapper.Map(productDto, product);
 
+            if (productDto.File != null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if (imageResult.Error != null)
+                    return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+
+                if (!string.IsNullOrEmpty(product.PublicId))
+                    await _imageService.DeleteImageAsync(product.PublicId);
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.PublicId = imageResult.PublicId;
+
+            }
+
             var result = await _context.SaveChangesAsync() > 0;
 
-            if(result) return NoContent();
+            if (result) return Ok(product);
 
-            return BadRequest(new ProblemDetails{Title="Problem updating the product"});
+            return BadRequest(new ProblemDetails { Title = "Problem updating the product" });
         }
 
         [Authorize(Roles = "Admin")]
@@ -93,15 +121,18 @@ namespace API.Controllers
         {
             var product = await _context.Products.FindAsync(id);
 
-            if(product == null) return NotFound();
+            if (product == null) return NotFound();
+
+            if (!string.IsNullOrEmpty(product.PublicId))
+                await _imageService.DeleteImageAsync(product.PublicId);
 
             _context.Products.Remove(product);
 
             var result = await _context.SaveChangesAsync() > 0;
 
-            if(result) return Ok();
+            if (result) return Ok();
 
-            return BadRequest(new ProblemDetails{Title = "Problem deleting the product"});
+            return BadRequest(new ProblemDetails { Title = "Problem deleting the product" });
         }
     }
 }
---
+++ API/Data/Migrations/20230627134846_PublicIdAdded.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PublicIdAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "e2098fb5-1e34-442f-8e26-483e3343ffe4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "5833b579-83e7-45ac-925a-106c0f417ea8");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Products");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "35c7cc45-06f5-4a89-b0a5-7a1f88f81b4d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "50982380-44da-494d-b0de-e04c3564cf72");
+        }
+    }
+}
